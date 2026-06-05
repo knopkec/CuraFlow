9e55d574054c3c97301ddfb07488109a8cad969a
Andreas Knopke <andreasknopke@gmail.com>
2026-06-05 07:01:56 +0000

    fix(master): remove unused Switch import in PayScaleTariffs

--- src/master/pages/MasterPayScaleTariffs.jsx
+++ src/master/pages/MasterPayScaleTariffs.jsx
@@ -6,7 +6,6 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Switch } from '@/components/ui/switch';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import {
   Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogDescription,
